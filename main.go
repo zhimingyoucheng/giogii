837f9ea7ad8d4d8c8f449da8acccdffdfaa7836c
eellff <48122487+eellff@users.noreply.github.com>
2022-08-19 08:11:20 +0800

    add note

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
 	var targetSocket string
 
 	flag.StringVar(&sourceUserInfo, "s", "admin:!QAZ2wsx", "")
-	flag.StringVar(&sourceSocket, "si", "172.16.76.105:16310", "")
+	flag.StringVar(&sourceSocket, "si", "172.16.128.13:16310", "")
 	flag.StringVar(&targetUserInfo, "t", "admin:!QAZ2wsx", "")
-	flag.StringVar(&targetSocket, "ti", "172.16.128.14:16310", "")
+	flag.StringVar(&targetSocket, "ti", "172.16.76.106:16310", "")
 	flag.Parse()
-	//fmt.Println(sourceUserInfo, sourceSocket, targetUserInfo, targetSocket)
+
 	check.ConfigInit(sourceUserInfo, sourceSocket, targetUserInfo, targetSocket)
 	check.DoCheck()
 
--- src/check/checkGtid.go
+++ src/check/checkGtid.go
@@ -55,46 +55,60 @@ func DoCheck() {
 	var masterGtid string
 	var slaveGtid string
 
-	if masterStatus.File != "" && slaveStatus.MasterLogFile != "" {
-		masterExecutedGtids := strings.Split(masterStatus.ExecutedGtidSet, ",")
-		for i := 0; i < len(masterExecutedGtids); i++ {
-			if strings.Contains(masterExecutedGtids[i], masterUuid) {
-				masterGtid = strings.Trim(masterExecutedGtids[i], "\n")
-				break
-			}
-		}
-
-		slaveExecutedGtids := strings.Split(slaveStatus.ExecutedGtidSet, ",")
-		for i := 0; i < len(slaveExecutedGtids); i++ {
-			if strings.Contains(slaveExecutedGtids[i], masterUuid) {
-				slaveGtid = strings.Trim(slaveExecutedGtids[i], "\n")
-				break
-			}
-		}
-		if strings.Contains(slaveGtid, masterGtid) {
-			rs -= 1
-		} else if strings.Contains(slaveGtid, "-") && strings.Contains(masterGtid, "-") {
-			slaveLastIndex := strings.LastIndex(slaveGtid, "-")
-			masterLastIndex := strings.LastIndex(masterGtid, "-")
-			if masterGtid[masterLastIndex+1:] == slaveGtid[slaveLastIndex+1:] {
-				rs -= 1
-			}
-		}
-
-		if strconv.Itoa(*masterStatus.Position) == strconv.Itoa(*slaveStatus.ReadMasterLogPos) {
-			rs -= 1
-		}
-
-		log.Printf("Source Cluster GTID：%s", masterGtid)
-		log.Printf("Target Cluster GTID：%s", slaveGtid)
-		log.Print("Source Cluster POS: ", *masterStatus.Position)
-		log.Print("Target Cluster POS: ", *slaveStatus.ReadMasterLogPos)
-
-		fmt.Println(rs)
-
-	} else {
+	// 如果两个语句的返回值里任何一个不包含binlog文件，直接返回2
+	if masterStatus.File == "" || slaveStatus.MasterLogFile == "" {
 		log.Printf("show master status / show slave status return null")
+		fmt.Println(rs)
+		return
 	}
+
+	// 如果slave读取的binlog文件和主库当前binlog文件不相等，说明延迟很大，直接返回2，不需要进行下面比较
+	if masterStatus.File != slaveStatus.MasterLogFile {
+		log.Printf("")
+		fmt.Println(rs)
+		return
+	}
+
+	// 获取主集群执行的gtid
+	masterExecutedGtids := strings.Split(masterStatus.ExecutedGtidSet, ",")
+	for i := 0; i < len(masterExecutedGtids); i++ {
+		if strings.Contains(masterExecutedGtids[i], masterUuid) {
+			masterGtid = strings.Trim(masterExecutedGtids[i], "\n")
+			break
+		}
+	}
+
+	// 获取备集群执行的gtid
+	slaveExecutedGtids := strings.Split(slaveStatus.ExecutedGtidSet, ",")
+	for i := 0; i < len(slaveExecutedGtids); i++ {
+		if strings.Contains(slaveExecutedGtids[i], masterUuid) {
+			slaveGtid = strings.Trim(slaveExecutedGtids[i], "\n")
+			break
+		}
+	}
+
+	// 这里的逻辑是判断主集群GTID是否和备集群GTID相等
+	if strings.Contains(slaveGtid, masterGtid) {
+		rs -= 1
+	} else if strings.Contains(slaveGtid, "-") && strings.Contains(masterGtid, "-") {
+		slaveLastIndex := strings.LastIndex(slaveGtid, "-")
+		masterLastIndex := strings.LastIndex(masterGtid, "-")
+		if masterGtid[masterLastIndex+1:] == slaveGtid[slaveLastIndex+1:] {
+			rs -= 1
+		}
+	}
+
+	// 这里的逻辑是判断主集群binlog点位是否和备集群点位相等
+	if strconv.Itoa(*masterStatus.Position) == strconv.Itoa(*slaveStatus.ReadMasterLogPos) {
+		rs -= 1
+	}
+
+	log.Printf("Source Cluster GTID：%s", masterGtid)
+	log.Printf("Target Cluster GTID：%s", slaveGtid)
+	log.Print("Source Cluster POS: ", *masterStatus.Position)
+	log.Print("Target Cluster POS: ", *slaveStatus.ReadMasterLogPos)
+
+	fmt.Println(rs)
 
 	defer func() {
 		MasterSqlScaleOperator.DoClose()
