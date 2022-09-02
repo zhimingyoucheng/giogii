8874958f4c218969a08d2ce0ac17adaa0204b79f
wangjingyu <jingyu.wang@greatdb.com>
2022-09-02 10:41:12 +0800

    noo

--- main.go
+++ main.go
@@ -26,10 +26,10 @@ func main() {
 	flag.StringVar(&sourceSocket, "si", "172.17.128.49:13336", "")
 	flag.StringVar(&targetUserInfo, "t", "admin:!QAZ2wsx", "")
 	flag.StringVar(&targetSocket, "ti", "172.17.128.13:16310", "")
-	flag.StringVar(&parameter, "c", "parameter", "")
+	flag.StringVar(&parameter, "c", "", "")
 	flag.Parse()
 
-	if strings.Trim(parameter, " ") == "parameter" {
+	if strings.Trim(parameter, " ") == "base" {
 		check.InitCheckParameterConf(sourceUserInfo, sourceSocket, "greatrds", targetUserInfo, targetSocket, "information_schema")
 		check.DoCheckParameter()
 	} else {
