6b080c88a0d93b026d7b22968410007f39962a63
wangjingyu <jingyu.wang@greatdb.com>
2022-11-10 05:22:02 +0800

    no

--- main.go
+++ main.go
@@ -13,17 +13,11 @@ func main() {
 	var targetSocket string
 	var parameter string
 
-	/*flag.StringVar(&sourceUserInfo, "s", "root:drACgwoqtM", "")
-	flag.StringVar(&sourceSocket, "si", "172.17.128.49:13336", "")
-	flag.StringVar(&targetUserInfo, "t", "wjy_root:Wjy123456", "")
-	flag.StringVar(&targetSocket, "ti", "rm-2ze5j9oqx3x70jzd94o.mysql.rds.aliyuncs.com:3306", "")
-	flag.StringVar(&parameter, "c", "8c32gb", "")*/
-
-	flag.StringVar(&sourceUserInfo, "s", "admin:!QAZ2wsx", "")
-	flag.StringVar(&sourceSocket, "si", "172.17.139.27:16310", "")
-	flag.StringVar(&targetUserInfo, "t", "root:!QAZ2wsx", "")
-	flag.StringVar(&targetSocket, "ti", "172.17.140.3:16310", "")
-	flag.StringVar(&parameter, "", "", "")
+	flag.StringVar(&sourceUserInfo, "s", "", "")
+	flag.StringVar(&sourceSocket, "si", "", "")
+	flag.StringVar(&targetUserInfo, "t", "", "")
+	flag.StringVar(&targetSocket, "ti", "", "")
+	flag.StringVar(&parameter, "c", "", "")
 
 	flag.Parse()
 
